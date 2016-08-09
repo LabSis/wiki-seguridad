6e6518d55154dce24425fa83ad3002c8df2532b5
Gochi <germannparisi@gmail.com>
2016-08-09 13:56:13 -0300

    Arreglo visualizacion de tecnicas cuando hay mas de 4.

--- index.php
+++ index.php
@@ -6,91 +6,6 @@ $sesion = Session::get_instance();
 
 ini_set("display_errors", 1);
 $tmpl_tecnicas = ApiBd::obtener_tecnicas();
-
-/*
-$tmpl_tecnicas = array(
-    array(
-        "nombre" => "Inyección",
-        "links" => array(
-            array(
-                "href" => "tecnica.php?id=1",
-                "nombre" => "Inyección SQL"
-            ),
-            array(
-                "href" => "",
-                "nombre" => "Inyección LDAP"
-            ),
-            array(
-                "href" => "",
-                "nombre" => "Inyección XML"
-            ),
-            array(
-                "href" => "",
-                "nombre" => "Inyección NoSQL"
-            )
-        )
-    ),
-    array(
-        "nombre" => "Sitios cruzados",
-        "links" => array(
-            array(
-                "href" => "tecnica.php?id=2",
-                "nombre" => "XSS"
-            ),
-            array(
-                "href" => "",
-                "nombre" => "Falsificación de peticiones en sitios cruzados (CSRF)"
-            )
-        )
-    ),
-    array(
-        "nombre" => "Control de acceso",
-        "links" => array(
-            array(
-                "href" => "tecnica.php?id=2",
-                "nombre" => "Pérdida de autenticación y gestión de sesiones"
-            ),
-            array(
-                "href" => "",
-                "nombre" => "Inexistente control de acceso a nivel de funcionalidades"
-            ),
-            array(
-                "href" => "",
-                "nombre" => "Exposición a datos sensibles"
-            ),
-            array(
-                "href" => "",
-                "nombre" => "Referencia directa insegura a objetos"
-            )
-        )
-    ),
-    array(
-        "nombre" => "Configuración",
-        "links" => array(
-            array(
-                "href" => "tecnica.php?id=2",
-                "nombre" => "Configuración de seguridad incorrecta"
-            ),
-            array(
-                "href" => "",
-                "nombre" => "Uso de componentes con vulnerabilidades conocidas"
-            )
-        )
-    ),
-    array(
-        "nombre" => "Inclusión",
-        "links" => array(
-            array(
-                "href" => "tecnica.php?id=2",
-                "nombre" => "LFI"
-            ),
-            array(
-                "href" => "",
-                "nombre" => "RFI"
-            )
-        )
-    )
-);*/
 ?>
 <!DOCTYPE html>
 <html>
@@ -106,31 +21,37 @@ $tmpl_tecnicas = array(
     <body>
         <main class="container">
             <h1>LabSis - Seg</h1>
-            <h3>Técnicas</h3>
+            <h3>Técnicas de ataques</h3>
             <div class="row">
                 <div class="col-sm-12">
-                    <div class="row">
-                        <?php foreach ($tmpl_tecnicas as $tmpl_tecnica): ?>
-                            <div class="col-sm-3">
-                                <div class="panel panel-default">
-                                    <div class="panel-heading">
-                                        <h3 class="panel-title"><?php echo $tmpl_tecnica["nombre"]; ?></h3>
-                                    </div>
-                                    <div class="panel-body">
-                                        <ul>
-                                            <?php foreach ($tmpl_tecnica["links"] as $link): ?>
-                                                <li>
-                                                    <a href="src/tecnica.php?id=<?php echo $link["href"]; ?>">
-                                                        <?php echo $link["nombre"]; ?>
-                                                    </a>
-                                                </li>
-                                            <?php endforeach; ?>
-                                        </ul>
-                                    </div>
+                    <?php $i = 1 ?>
+                    <?php foreach ($tmpl_tecnicas as $tmpl_tecnica): ?>
+                        <?php if ($i % 4 == 1): ?>
+                            <div class="row">
+                        <?php endif; ?>
+                        <div class="col-sm-3">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h3 class="panel-title"><?php echo $tmpl_tecnica["nombre"]; ?></h3>
+                                </div>
+                                <div class="panel-body">
+                                    <ul>
+                                        <?php foreach ($tmpl_tecnica["links"] as $link): ?>
+                                            <li>
+                                                <a href="src/tecnica.php?id=<?php echo $link["href"]; ?>">
+                                                    <?php echo $link["nombre"]; ?>
+                                                </a>
+                                            </li>
+                                        <?php endforeach; ?>
+                                    </ul>
                                 </div>
                             </div>
-                        <?php endforeach; ?>
-                    </div>
+                        </div>
+                        <?php if ($i % 4 == 0): ?>
+                            </div>
+                        <?php endif; ?>
+                        <?php $i++ ?>
+                    <?php endforeach; ?>
                 </div>
             </div>
         </main>
