fd2025d074b1820321ca18edc0a9b513609345db
Fede Bertola <federicojbertola@gmail.com>
2019-04-23 11:08:45 -0300

    Agrego validación si existe la variable  para mostrar la cantidad de visitas.

--- tmpl/maquetado/menu.tmpl.php
+++ tmpl/maquetado/menu.tmpl.php
@@ -1,7 +1,7 @@
 <div class="row">
     <div class="col-sm-12">
         <a href="<?php echo (isset($LINK_VOLVER)) ? $LINK_VOLVER: $WEB_PATH; ?>">Volver</a> |
-        <?php if ($sesion->is_active()): ?>
+        <?php if ($sesion->is_active() && isset($tmpl_contenedor)): ?>
             <span>Visitas:</span> <span><?php echo $tmpl_contenedor["visitas"] ?></span>
         <?php endif; ?>
         <hr class="division-line-menu"/>
