eda9969f2763d46f5bfc3bc565fd9ab66935e91c
Germán Parisi <germannparisi@gmail.com>
2019-06-30 14:47:47 -0300

    Arreglo enlace en el footer

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
     <div class="footer-left">
         <p>Base de datos creada por el equipo de seguridad informática y desarrollo.</p>
         <p>
-            <a href="https://labsys.frc.utn.edu.ar/investigacion" >UTF - FRC - LabSis</a>
+            <a href="https://labsys.frc.utn.edu.ar/investigacion/" >UTF - FRC - LabSis</a>
         </p>
     </div>
 </footer>
