1ce1b85b5becaf1ecc0cbea48c63e26afdf6620d
ileana maricel <ileana@arya>
2019-08-14 15:46:41 -0300

    ckeditor 4.12 - nuevos pluginns - codesnippet, images, videos, html, etc...

--- config.php
+++ config.php
@@ -15,7 +15,7 @@ header('Content-Type: text/html; charset=utf-8');
     // Rutas absolutas
 $PROTOCOL = 'http://';
 $SERVER_PATH = dirname(__FILE__) . '/';
-$WEB_PATH = $PROTOCOL . $_SERVER['HTTP_HOST'] . '/labsis_seg/';
+$WEB_PATH = $PROTOCOL . $_SERVER['HTTP_HOST'] . '/wiki-seguridad/';
 $LOGIN_PATH = $WEB_PATH;
 $PUBLIC_PATH = $SERVER_PATH . "public/";
 
@@ -100,8 +100,8 @@ spl_autoload_register(function ($nombre_clase) {
 
 /*** Base de datos ***/
 $DB_HOST = 'localhost';
-$DB_USER = 'root';
-$DB_PASS = '12345678';
+$DB_USER = 'ile';
+$DB_PASS = 'ileile';
 $DB_NAME = 'labsis_seg';
 
 Conexion::set_default_conexion($DB_NAME, Conexion::init($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME, DEV_MODE));
--- js/ckeditor/build-config.js
+++ js/ckeditor/build-config.js
@@ -1,6 +1,6 @@
 ﻿/**
- * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md or http://ckeditor.com/license
+ * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/license
  */
 
 /**
@@ -10,13 +10,13 @@
  * If you would like to build CKEditor online again
  * (for example to upgrade), visit one the following links:
  *
- * (1) http://ckeditor.com/builder
+ * (1) https://ckeditor.com/cke4/builder
  *     Visit online builder to build CKEditor from scratch.
  *
- * (2) http://ckeditor.com/builder/268899b4e81b170a30748f556d1d1f9e
+ * (2) https://ckeditor.com/cke4/builder/64bf8c6a8e2ec3ea73c5b6eb9dc03c00
  *     Visit online builder to build CKEditor, starting with the same setup as before.
  *
- * (3) http://ckeditor.com/builder/download/268899b4e81b170a30748f556d1d1f9e
+ * (3) https://ckeditor.com/cke4/builder/download/64bf8c6a8e2ec3ea73c5b6eb9dc03c00
  *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
  *
  * NOTE:
@@ -25,49 +25,84 @@
  */
 
 var CKBUILDER_CONFIG = {
-	skin: 'moono',
-	preset: 'basic',
+	skin: 'moono-lisa',
+	preset: 'standard',
 	ignore: [
-		'.bender',
-		'bender.js',
-		'bender-err.log',
-		'bender-out.log',
-		'dev',
 		'.DS_Store',
+		'.bender',
 		'.editorconfig',
 		'.gitattributes',
 		'.gitignore',
-		'gruntfile.js',
 		'.idea',
 		'.jscsrc',
 		'.jshintignore',
 		'.jshintrc',
-		'less',
 		'.mailmap',
+		'.npm',
+		'.travis.yml',
+		'bender-err.log',
+		'bender-out.log',
+		'bender.ci.js',
+		'bender.js',
+		'dev',
+		'gruntfile.js',
+		'less',
 		'node_modules',
 		'package.json',
-		'README.md',
 		'tests'
 	],
 	plugins : {
+		'a11yhelp' : 1,
 		'about' : 1,
 		'basicstyles' : 1,
+		'blockquote' : 1,
 		'clipboard' : 1,
+		'codesnippet' : 1,
+		'colorbutton' : 1,
+		'contextmenu' : 1,
+		'easyimage' : 1,
+		'elementspath' : 1,
 		'enterkey' : 1,
 		'entities' : 1,
+		'filebrowser' : 1,
 		'floatingspace' : 1,
 		'font' : 1,
+		'format' : 1,
+		'horizontalrule' : 1,
+		'htmlwriter' : 1,
+		'image' : 1,
+		'image2' : 1,
 		'indentlist' : 1,
+		'justify' : 1,
 		'link' : 1,
 		'list' : 1,
+		'magicline' : 1,
+		'maximize' : 1,
+		'pastefromword' : 1,
+		'pastetext' : 1,
+		'removeformat' : 1,
+		'resize' : 1,
+		'scayt' : 1,
+		'showborders' : 1,
+		'sourcearea' : 1,
+		'specialchar' : 1,
+		'stylescombo' : 1,
+		'tab' : 1,
+		'table' : 1,
+		'tableresize' : 1,
+		'tableselection' : 1,
+		'tabletools' : 1,
 		'toolbar' : 1,
 		'undo' : 1,
-		'wysiwygarea' : 1
+		'uploadimage' : 1,
+		'wsc' : 1,
+		'wysiwygarea' : 1,
+		'youtube' : 1
 	},
 	languages : {
 		'en' : 1,
 		'es' : 1,
-		'fr-ca' : 1,
-		'pt-br' : 1
+		'fr' : 1,
+		'pt' : 1
 	}
 };
